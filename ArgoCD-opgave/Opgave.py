7da59b811504762dbf42c06de4145a2dae0cae0e
Skole-Zafer <zaf.acil@gmail.com>
2026-06-03 11:16:19 +0200

    test3

--- ArgoCD-opgave/Opgave.py
+++ ArgoCD-opgave/Opgave.py
@@ -4,15 +4,15 @@ import time
 myMessage = "self destructing in"
 
 
-print("Hello, World!")
+print(("Hello, World!"), flush=True)
 
 for i in range(10, -1, -1):
     time.sleep(1)
-    print(myMessage + " " + str(i) + " seconds...")
+    print((myMessage + " " + str(i) + " seconds..."), flush=True)
 
 value = i
 
 if value == 0:
-    print("BOOM! Self destructed!")
+    print(("BOOM! Self destructed!"), flush=True)
 
 #testtest
